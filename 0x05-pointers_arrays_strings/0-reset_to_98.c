d3fc8c41a4cdca7136a3d32c290a31c55bf63bb8
Mariamugo <shee.mw70@gmail.com>
2023-12-08 18:40:18 -0800

    updates pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function updates pounter value to 98
+ *
+ * @n: parameter argument takenby function
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
